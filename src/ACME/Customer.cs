f2266bd2515c75c4d846747ae80332769743b5c9
Seth NeJame <sethnejame@gmail.com>
2019-08-12 19:18:35 +0200

    Adding Customer class w/ basic props (email, name etc)

---
+++ src/ACME/Customer.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ACME
+{
+  public class Customer
+  {
+    // the DB will set the ID automatically
+    public int CustomerId { get; private set; }
+
+    public string Email { get; set; }
+    // set/get methods and backing field all in one
+    public string FirstName { get; set; }
+    // this is a backing field for the prop declared in LastName
+    private string _lastName;
+    // the prop below is stored in the backing field above via the get/set accessors
+    public string LastName
+    {
+      get
+      {
+        return _lastName;
+      }
+      set
+      {
+        _lastName = value;
+      }
+    }
+
+  }
+}
